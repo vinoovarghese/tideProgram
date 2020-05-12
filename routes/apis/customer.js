a30e9f22b4fee5dacb2399921040181754dee4f8
Varghese <Vinoo.Varghese@dell.com>
2020-05-12 15:52:23 +0530

    Routes added for retieving all customers,single customer,deleting a single customer

--- routes/apis/customer.js
+++ routes/apis/customer.js
@@ -16,7 +16,7 @@ router.get("/",(req,res)=>
 router.post(
   "/",
   [
-    //Validations to be verified using .
+    //Validations as to whether it's a valid email address and that name/address/currency are not blank.
 
     check("name", "Name is required.").not().isEmpty(),
     check("email", "Please enter a valid email.").isEmail(),
@@ -31,7 +31,7 @@ router.post(
 
     const { name, email, address,currency } = req.body;
 
-    // First,find out whether the user already exists using the email.
+    // First,find out whether the customer already exists using the email.
      try {
       let customer = await Customer.findOne({
         email,
@@ -51,7 +51,7 @@ router.post(
         customer = await customer.save();
        
        console.log(customer.name + " was succesfully registered !!!");
-       res.json({message:customer.name + " was succesfully registered !!!",Object:customer});
+       res.json({message:customer.name + " was succesfully registered !!!",customer});
        
        
       }
@@ -61,6 +61,66 @@ router.post(
     }
   }
 );
+
+//Route to get the list of all customers present
+
+router.get("/allCustomers", async (req, res) => {
+  try {
+     const allCustomers = await Customer.find().populate("customer");
+     res.json({message:"List of customers below : " , allCustomers});
+   
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).message("error");
+  }
+});
+
+// Route to get a particular customer provided you give the customer id
+
+ router.get("/:custId", async (req, res) => {
+  try {
+
+    //Find out whether the customer exists or not
+
+    const customerFound = await Customer.findById(req.params.custId);
+    
+    if (!customerFound) {
+         return res.status(404).send({ message: "No such customer with the customerid  : " + req.params.custId + " exists !!!"});
+    }
+    res.json({message:"Customer details below : " , customerFound});
+        
+  } catch (error) {
+    console.log(error);
+    res.status(500).message("error");
+  }
+});
+
+// Route to delete a customer 
+
+router.delete("/:custId",async (req, res) => {
+
+  try {
+    
+    //Find out whether the customer exists or not
+
+    const customerToBeDeleted = await Customer.findById({_id: req.params.custId});
+    if(!customerToBeDeleted) {
+       
+      return res.status(404).send({ message: "No such customer with the customerid  :  " + req.params.custId + " exists !!!" });
+    }
+
+    const deletedCustomer = await Customer.findOneAndRemove({ _id: req.params.custId });
+    console.log("Customer was deleted " + deletedCustomer);
+    res.json({message: deletedCustomer.name + " was deleted !!! " , deletedCustomer});
+
+
+  } catch (error) {
+    
+    console.log(error);
+    res.status(500).message("error");
+  }
+
+});
 
 
 module.exports=router;
