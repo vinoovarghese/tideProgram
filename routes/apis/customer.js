46dcb3ac42c83a2b0f6c62c40827fe2648bb442e
Varghese <Vinoo.Varghese@dell.com>
2020-05-12 13:02:12 +0530

    Changes to server.js to ensure that json is read

--- routes/apis/customer.js
+++ routes/apis/customer.js
@@ -14,51 +14,53 @@ router.get("/",(req,res)=>
 
 // Create a new Customer
 router.post(
-    "/",
-    [
-      
-      check("name", "Name is required.").not().isEmpty(),
-      check("email", "Please enter a valid email.").isEmail(),
-      check("address","Please enter a valid address.").isEmpty()
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      const { name, email,address,currency } = req.body;
-  
-      // First,find out whether the customer already exists using the email.
-       try {
-        let customer = await Customer.findOne({
-          email,
-        });
-        if (customer) {
-          return res.status(400).json({ errors: [{ message: "Customer already exists ." }] });
-        } else {
-          
-  
-          customer = new Customer({
-            name,
-            email,
-            address,
-            currency
-          });
-        
-          var customerName = await customer.save();
-         
-          console.log(customer.name + " was succesfully registered !!!");
-          res.json( { message: customer.name + " successfully registered !!! ", customer } ) ;
+  "/",
+  [
+    //Validations to be verified using .
 
-         res.json(customer);
-         
-        }
-      } catch (err) {
-        res.status(500).send("error");
-      }
+    check("name", "Name is required.").not().isEmpty(),
+    check("email", "Please enter a valid email.").isEmail(),
+    check("address", "Address is required.").not().isEmpty(),
+    check("currency", "Currency is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
+
+    const { name, email, address,currency } = req.body;
+
+    // First,find out whether the user already exists using the email.
+     try {
+      let customer = await Customer.findOne({
+        email,
+      });
+      if (customer) {
+        return res.status(400).json({ errors: [{ message: "Customer already exists ." }] });
+      } else {
+        
+
+        customer = new Customer({
+          name,
+          email,
+          address,
+          currency
+        });
+      
+        customer = await customer.save();
+       
+       console.log(customer.name + " was succesfully registered !!!");
+       res.
+       res.json(customer);
+       
+      }
+    } catch (err) {
+      console.log("Error has occured " + err.message);
+      res.status(500).send("error");
+    }
+  }
+);
 
 
 module.exports=router;
